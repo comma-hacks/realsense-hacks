37921b6b5115c23490090620b2dcedfc4b265f85
Keyvan Fatehi <keyvanfatehi@gmail.com>
2023-01-14 15:03:28 -0800

    integrating wasd input method and statis

--- main.cpp
+++ main.cpp
@@ -6,10 +6,16 @@
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Geometry>
 
+#include "robot.h"
+#include "wasd.h"
+
 using namespace Eigen;
 
 int main(int argc, char **argv)
 {
+  // Setup our input method (keyboard stdin)
+  setupTerminal();
+
   // Declare RealSense pipeline, encapsulating the actual device and sensors
   rs2::pipeline pipe;
   // Create a configuration for configuring the pipeline with a non default profile
@@ -19,7 +25,9 @@ int main(int argc, char **argv)
   // Start pipeline with chosen configuration
   pipe.start(cfg);
 
-  // Main loop
+  Vector3d desired_position(0.0, 0.0, 0.0);
+  Quaternion<double> desired_rotation(1.0, 0.0, 0.0, 0.0);
+
   while (true)
   {
     // Wait for the next set of frames from the camera
@@ -28,20 +36,60 @@ int main(int argc, char **argv)
     auto f = frames.first_or_default(RS2_STREAM_POSE);
     // Cast the frame to pose_frame and get its data
     auto pose = f.as<rs2::pose_frame>().get_pose_data();
+    Vector3d current_position(pose.translation.x, pose.translation.y, pose.translation.z);
+    Quaternion<double> current_rotation(pose.rotation.w, pose.rotation.x, pose.rotation.y, pose.rotation.z);
 
-    int mult = 1000;
+    MOVE mov = checkTerminalInput();
+    switch (mov)
+    {
+    case MOVE_FORWARD:
+      robotMoveForward();
+      desired_position = current_position;
+      break;
+    case MOVE_BACKWARD:
+      robotMoveBackward();
+      desired_position = current_position;
+      break;
+    case ROTATE_LEFT:
+      robotRotateLeft();
+      desired_rotation = current_rotation;
+      break;
+    case ROTATE_RIGHT:
+      robotRotateRight();
+      desired_rotation = current_rotation;
+      break;
+    case NONE: {
+        // Calculate position and rotation errors
+        Vector3d position_error = desired_position - current_position;
+        Quaternion<double> rotation_error = desired_rotation * current_rotation.conjugate();
 
-    //auto x = pose.translation.x * mult;
-    //auto y = pose.translation.y * mult;
-    //auto z = pose.translation.z * mult;
+        // Print position error and yaw error
+        double yaw_error = 2.0 * std::atan2(rotation_error.vec().z(), rotation_error.w());
+        std::cout << "Position Error: (" << position_error.x() << ", " << position_error.y() << ") Yaw Error: " << yaw_error << std::endl;
 
-    Quaternion q(pose.rotation.w, pose.rotation.x, pose.rotation.y, pose.rotation.z);
-    auto euler = q.toRotationMatrix().eulerAngles(0, 1, 2);
-    std::cout << "{\"yaw\":" << euler[0] << ",\"pitch\":" << euler[1] << ",\"roll\":" << euler[2] << "}" << std::endl;
+        // Apply inputs to the robot's web API based on the position error
+        if (position_error.x() > 0.01)
+          robotMoveBackward();
+        else if (position_error.x() < -0.01)
+          robotMoveForward();
+        if (position_error.y() > 0.01)
+          robotRotateLeft();
+        else if (position_error.y() < -0.01)
+          robotRotateRight();
 
+        // Apply inputs to the robot's web API based on the rotation error
+        if (yaw_error > 0.01)
+          robotRotateLeft();
+        else if (yaw_error < -0.01)
+          robotRotateRight();
+    }
+    default:
+      break;
+    }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    // std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
+  resetTerminal();
   return 0;
 }
---
+++ wasd.h
@@ -0,0 +1,53 @@
+#include <termios.h>
+#include <unistd.h>
+#include <cstdio>
+
+enum MOVE
+{
+  NONE = 0,
+  MOVE_FORWARD,
+  MOVE_BACKWARD,
+  ROTATE_LEFT,
+  ROTATE_RIGHT
+};
+
+struct termios old_tio, new_tio;
+
+void setupTerminal()
+{
+  tcgetattr(STDIN_FILENO, &old_tio);
+  new_tio = old_tio;
+  new_tio.c_lflag &= ~ICANON;
+  new_tio.c_lflag &= ~ECHO;
+  tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void resetTerminal()
+{
+  tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
+}
+
+MOVE checkTerminalInput()
+{
+  if (getchar() != EOF)
+  {
+    char key = getchar();
+    if (key == 'w' || key == 'W' || key == 'i' || key == 65)
+    {
+      return MOVE_FORWARD;
+    }
+    else if (key == 's' || key == 'S' || key == 'k' || key == 66)
+    {
+      return MOVE_BACKWARD;
+    }
+    else if (key == 'a' || key == 'A' || key == 'j' || key == 68)
+    {
+      return ROTATE_LEFT;
+    }
+    else if (key == 'd' || key == 'D' || key == 'l' || key == 67)
+    {
+      return ROTATE_RIGHT;
+    }
+  }
+  return NONE;
+}
